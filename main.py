e522744e3f742c3ff77bd96387337ecb1508b9d7
AAT <AAT@example.com>
2024-04-19 19:01:44 -0800

    Add links, speicify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import requests 
+import requests
+from send_email import send_email 
 
+topic = "Tesla"
 api_key="88970df1b20840ef82308e861c561b0e"
-url = "https://newsapi.org/v2/everything?q=apple&from=2024-04-15&to=2024-04-15&sortBy=popularity&apiKey=88970df1b20840ef82308e861c561b0e"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2024-03-20&" \
+      "sortBy=publishedAt&"\
+      "apiKey=88970df1b20840ef82308e861c561b0e&" \
+      "language=en"
 
 
 # Make request
@@ -9,12 +16,22 @@ request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-print(content)
-
-
+# print(content)
+message = ""
 # Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+for article in content["articles"][:20]:
+    # message = message + f"""\
+             
+    # Title: {article["title"]}
+    # Description: {article["description"]}    
+    
+    # """
+    if article["title"] and article["description"] is not None:
+        message = "Subject: Today's news" + "\n" + message + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
+
+message = message.encode("utf-8")
+send_email(message)
+    # print(article["title"])
+    # print(article["description"])
 
 
